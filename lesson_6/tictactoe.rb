1c2b16784f6f4262a2d4837e8eca532999d00230
dantrovato <plasticsoup@yahoo.co.uk>
2018-08-27 15:42:11 +0100

    add player and computer moves so that they cannot use a previously turned square

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,8 +1,9 @@
+require 'pry'
+require 'pry-byebug'
+
 # create board/ display board
-row_choices = %w(1 2 3)
-column_choices = %w(a b c)
-get_row = ''
-get_column = ''
+square_choices = %w(0 1 2 3 4 5 6 7 8 9)
+get_square = ''
 BOARD = [
   ['-', '-', '-'],
   ['-', '-', '-'],
@@ -27,22 +28,31 @@ def computer_turn
   case p computer_options.sample
   when 0
     BOARD[0][0] = 'o'
+    AVAILABLE_SLOTS[0] = nil
   when 1
     BOARD[0][1] = 'o'
+    AVAILABLE_SLOTS[1] = nil
   when 2
     BOARD[0][2] = 'o'
+    AVAILABLE_SLOTS[2] = nil
   when 3
     BOARD[1][0] = 'o'
+    AVAILABLE_SLOTS[3] = nil
   when 4
     BOARD[1][1] = 'o'
+    AVAILABLE_SLOTS[4] = nil
   when 5
     BOARD[1][2] = 'o'
+    AVAILABLE_SLOTS[5] = nil
   when 6
     BOARD[2][0] = 'o'
+    AVAILABLE_SLOTS[6] = nil
   when 7
     BOARD[2][1] = 'o'
+    AVAILABLE_SLOTS[7] = nil
   when 8
     BOARD[2][2] = 'o'
+    AVAILABLE_SLOTS[8] = nil
   end
 
   print_board()
@@ -51,75 +61,54 @@ end
 print_board()
 
 # get user input
-
-# row
 while game_on
 
   if !BOARD[0].include?('-') && !BOARD[1].include?('-') && !BOARD[2].include?('-')
     puts 'AAAAAAANDA IZZA TIE!!!!'
-    game_on = false
+    # game_on = false
+    break
   end
 
   # get row
   loop do
-    puts 'Enter your row: 1, 2 or 3'
-    get_row = gets.chomp
-    p get_row
-    if row_choices.include?(get_row)
+    puts 'Enter your square: 1 to 9'
+    get_square = gets.chomp
+    p get_square
+    if square_choices.include?(get_square) && AVAILABLE_SLOTS[get_square.to_i - 1] != nil
       break
     else
       puts "Invalid choice. Try again, It's not hard."
     end
   end
 
-  # get column
-  loop do
-    puts 'Enter column: a b or c'
-    get_column = gets.chomp
-    if column_choices.include?(get_column)
-      break
-    else
-      puts "Invalid choice. Try again. You can do it. Probably."
-    end
-  end
-
-  # mark square
-  case get_row
+  case get_square
   when '1'
-    if get_column == 'a'
-      BOARD[0][0] = 'x'
-      AVAILABLE_SLOTS[0] = nil
-    elsif get_column == 'b'
-      BOARD[0][1] = 'x'
-      AVAILABLE_SLOTS[1] = nil
-    elsif get_column == 'c'
-      BOARD[0][2] = 'x'
-      AVAILABLE_SLOTS[2] = nil
-    end
+    BOARD[0][0] = 'x'
+    AVAILABLE_SLOTS[0] = nil
   when '2'
-    if get_column == 'a'
-      BOARD[1][0] = 'x'
-      AVAILABLE_SLOTS[3] = nil
-    elsif get_column == 'b'
-      BOARD[1][1] = 'x'
-      AVAILABLE_SLOTS[4] = nil
-    elsif get_column == 'c'
-      BOARD[1][2] = 'x'
-      AVAILABLE_SLOTS[5] = nil
-    end
-
+    BOARD[0][1] = 'x'
+    AVAILABLE_SLOTS[1] = nil
   when '3'
-    if get_column == 'a'
-      BOARD[2][0] = 'x'
-      AVAILABLE_SLOTS[6] = nil
-    elsif get_column == 'b'
-      BOARD[2][1] = 'x'
-      AVAILABLE_SLOTS[7] = nil
-    elsif get_column == 'c'
-      BOARD[2][2] = 'x'
-      AVAILABLE_SLOTS[8] = nil
-    end
-
+    BOARD[0][2] = 'x'
+    AVAILABLE_SLOTS[2] = nil
+  when '4'
+    BOARD[1][0] = 'x'
+    AVAILABLE_SLOTS[3] = nil
+  when '5'
+    BOARD[1][1] = 'x'
+    AVAILABLE_SLOTS[4] = nil
+  when '6'
+    BOARD[1][2] = 'x'
+    AVAILABLE_SLOTS[5] = nil
+  when '7'
+    BOARD[2][0] = 'x'
+    AVAILABLE_SLOTS[6] = nil
+  when '8'
+    BOARD[2][1] = 'x'
+    AVAILABLE_SLOTS[7] = nil
+  when '9'
+    BOARD[2][2] = 'x'
+    AVAILABLE_SLOTS[8] = nil
   else
     puts 'Invalid input'
   end
