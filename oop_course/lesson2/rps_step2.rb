b47be1b09e0a3b5aa11757a21606a292049d2fc9
Dani Mal <Danimal@fava.com>
2019-05-31 20:53:16 +0100

    add display_winner method and complete step 2

--- oop_course/lesson2/rps_step2.rb
+++ oop_course/lesson2/rps_step2.rb
@@ -1,3 +1,4 @@
+# DISPLAY WINNER
 CHOICES = %w(rock paper scissors)
 
 class Player
@@ -42,11 +43,28 @@ class RPSGame
     puts "Thanks for playing"
   end
 
+  def display_winner
+    puts "You chose #{human.move}"
+    puts "Computer chose #{computer.move}"
+    puts "It's a tie" if human.move == computer.move
+    case human.move
+    when 'rock'
+      puts "You won" if computer.move == 'scissors'
+      puts "Computer won" if computer.move == 'paper'
+    when 'paper'
+      puts "You won" if computer.move == 'rock'
+      puts "Computer won" if computer.move == 'scissors'
+    when 'scissors'
+      puts "You won" if computer.move == 'paper'
+      puts "Computer won" if computer.move == 'rock'
+    end
+  end
+
   def play
     display_welcome_message
     human.choice
     computer.choice
-    # declare_winner
+    display_winner
     display_goodbye_message
   end
 end
