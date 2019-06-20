d06bc43786d2c54aa0e6598f63755c2ba8594e75
Dani Mal <Danimal@fava.com>
2019-06-20 21:28:27 +0100

    complete essential game functionality

--- oop_course/lesson5/21.rb
+++ oop_course/lesson5/21.rb
@@ -1,44 +1,96 @@
 require 'pry'
 
 class Participant
-  attr_accessor :cards_held
+  attr_reader :cards_held, :deck
 
-  def initialize
+  def initialize(deck)
     @cards_held = []
+    @deck = deck
   end
 
-  def hit
+  def calculate_total(cards)
+    sum = 0
 
-  end
+    cards.each do |value|
+      if value == "ace"
+        sum += 11
+      elsif value.to_i == 0
+        sum += 10
+      else
+        sum += value.to_i
+      end
+    end
 
-  def stay
+    values.select { |value| value == 'ace' }.count.times do
+      sum -= 10 if sum > 21
+    end
 
-  end
-
-  def busted?
-
+    sum
   end
 
   def total
+    self.calculate_total(self.values)
+  end
+
+  def hit
     # binding.pry
-    values = cards_held.map { |card| card[1] } # => ex. ["ace", "7"]
+    cards_held << deck.get_card
+    puts "your cards now are now #{cards_held} worth #{total} "
+  end
+
+  def values
+     cards_held.map { |card| card[1] } # => ex. ["ace", "7"]
+  end
+
+  def is_bust?
+    total > 21
   end
 end
 
 class Player < Participant
+  def move
+    answer = nil
+    loop do
+      if total < 21
+        puts "Would you like to hit or stay? (h/s) "
 
+        loop do
+          answer = gets.chomp
+          break if %w(h s).include?(answer)
+          puts "Sorry. Invalid answer. 'h' or 's'."
+        end
+
+        if answer == 'h'
+          hit
+        else
+          puts "You stayed."
+          break
+        end
+      elsif total == 21
+        puts "Perfect score!"
+        break
+      else
+        puts "you've gone bust!"
+        break
+      end
+    end
+  end
 end
 
 class Dealer < Participant
 
-  # attr_reader :deck
-  #
-  # def initialize
-  #   @deck = Deck.new
-  # end
-  #
+  def move
+    loop do
+      if total < 17
+        cards_held << deck.get_card
+      end
+      break if total >= 17
+    end
+    puts "Dealer total is #{total}"
+  end
+
   def initial_total
-    [total[0]]
+    [values[0]]
   end
 end
 
@@ -46,52 +98,29 @@ class Deck
   SUITS = %w(hearts clubs diamonds spades)
   VALUES = %w(2 3 4 5 6 7 8 9 10 jack queen king ace)
 
-  attr_reader :cards, :player, :dealer
+  attr_reader :cards
 
   def initialize
     @cards = SUITS.product(VALUES).shuffle
-    @player = Player.new
-    @dealer = Dealer.new
+
   end
 
-  def deal_cards
-    2.times { player.cards_held << cards.shift.flatten }
-    2.times { dealer.cards_held << cards.shift.flatten }
-  end
-
-  def display_cards
-    puts "Your cards are #{player.cards_held}, for a total of: #{calculate_total(player.total)}" # => ex. [["diamonds", "3"], ["hearts", "6"]]
-    # binding.pry
-    puts "Dealer cards are #{dealer.cards_held[0]}, worth #{calculate_total(dealer.initial_total)} and ??"
+  def get_card
+    cards.shift
   end
 
   def to_s
     cards
   end
-
-  def calculate_total(cards)
-    total = 0
-
-    cards.each do |value|
-      if value == "ace"
-        total += 11
-      elsif value.to_i == 0
-        total += 10
-      else
-        total += value.to_i
-      end
-    end
-
-    total
-  end
 end
 
 class Game
-  attr_reader :deck
+  attr_reader :deck, :player, :dealer
 
   def initialize
-    # @dealer = Dealer.new
     @deck = Deck.new
+    @player = Player.new(deck)
+    @dealer = Dealer.new(deck)
   end
 
   def display_welcome_message
@@ -102,10 +131,41 @@ class Game
     puts "Thanks for playing."
   end
 
+  def deal_cards
+    2.times do
+      player.cards_held << deck.get_card
+      dealer.cards_held << deck.get_card
+    end
+  end
+
+  def display_cards
+    puts "Your cards are #{player.cards_held}, for a total of: #{player.total}" # => ex. [["diamonds", "3"], ["hearts", "6"]]
+    # binding.pry
+    puts "Dealer cards are #{dealer.cards_held[0]}, worth #{dealer.calculate_total(dealer.initial_total)} and ??"
+  end
+
+  def declare_winner
+    if player.is_bust?
+      puts "You busted. Dealer wins."
+    elsif dealer.is_bust?
+      puts "Dealer busted. Player wins."
+    elsif player.total > dealer.total
+      puts "Player wins."
+    elsif player.total < dealer.total
+      puts "Dealer wins."
+    else
+      puts "It's a tie!"
+    end
+  end
+
   def start
+    system 'clear'
     display_welcome_message
-    deck.deal_cards
-    deck.display_cards
+    deal_cards
+    display_cards
+    player.move
+    dealer.move
+    declare_winner
     display_goodbye_message
   end
 end
